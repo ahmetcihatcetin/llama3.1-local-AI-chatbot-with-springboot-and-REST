7ed91b317fd75d504dacd476c62169775d4371a3
ahmetcihatcetin <ahmetcihatcetin@outlook.com>
2025-04-11 11:05:35 +0300

    initialSetup branch created, project been created by spring initializr, first build been generated.

---
+++ ai_chatbot_restful/src/main/java/com/llama3_1_springboot3_4_4_java/ai_chatbot_restful/AiChatbotRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.llama3_1_springboot3_4_4_java.ai_chatbot_restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiChatbotRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiChatbotRestfulApplication.class, args);
+	}
+
+}
