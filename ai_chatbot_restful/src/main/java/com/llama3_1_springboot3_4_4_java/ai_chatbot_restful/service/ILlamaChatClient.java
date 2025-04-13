67ef7c3855529a5f65ea138999f927826c779050
ahmetcihatcetin <ahmetcihatcetin@outlook.com>
2025-04-13 15:25:59 +0300

    A new build with Gradle. Pay attention when and if mergering with main.

--- ai_chatbot_restful/src/main/java/com/llama3_1_springboot3_4_4_java/ai_chatbot_restful/service/ILlamaChatClient.java
+++
@@ -1,5 +0,0 @@
-package com.llama3_1_springboot3_4_4_java.ai_chatbot_restful.service;
-
-public interface ILlamaChatClient {
-    String generateChatMessage(String prompt);
-}
--- ai_chatbot_restful/src/main/java/com/llama3_1_springboot3_4_4_java/ai_chatbot_restful/service/ISpringLlamaAiService.java
+++
@@ -1,7 +0,0 @@
-package com.llama3_1_springboot3_4_4_java.ai_chatbot_restful.service;
-
-import com.llama3_1_springboot3_4_4_java.ai_chatbot_restful.response.SpringLlamaAiResponse;
-
-public interface ISpringLlamaAiService {
-    SpringLlamaAiResponse   generateMessage(String prompt);
-}
